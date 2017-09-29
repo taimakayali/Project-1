27317c39acc300774315cb8e8eb7217d231a4652
taimakayali <taimakayali@ufl.edu>
2017-09-29 18:37:45 -0400

    Black Jack Game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,118 +7,96 @@ public class Main {
     public static void main(String[] args) {
 
         int numGame = 1;
+        boolean continuePlaying = true;
 
-        System.out.println("START GAME # " + numGame++);
+        while (continuePlaying == true) {
 
-        Random hand = new Random();
-        int card = hand.nextInt((13 - 1) + 1) + 1;
+            System.out.println("START GAME # " + numGame++);
 
-        int totalHand = 0;
-        totalHand = totalHand + card;
+            Random hand = new Random();
+            int card = hand.nextInt((13 - 1) + 1) + 1;
 
-        int totalHandAces = 0; // total hand values for Face Cards and Aces
+            int totalHand = 0;
+            totalHand = totalHand + card;
 
-
-        int totalHandFaceCards = 0;
-        totalHandFaceCards = totalHandAces + 10; // adds 10 to player's hand
-
-        int handAceCard = 0;
-        handAceCard = totalHandAces + 1; // adds 1 to player's hand
-
-        int playerWins = 0;
-        int dealerWins = 0;
-        int playersTie = 0;
+            int totalHandAces = 0; // total hand values for Face Cards and Aces
 
 
-        if (card == 1) {
-            System.out.println("Your card is a ACE!");
-            System.out.println("Your hand is " + handAceCard + "!");
-        } else if ((card > 1) && (card < 11)) {
-            System.out.println("Your card is a " + card + "!");
-            System.out.println("Your hand is " + totalHand + "!");
-        } else if (card == 11) {
-            System.out.println("Your card is a JACK!");
-            System.out.println("Your hand is " + totalHandFaceCards + "!");
-        } else if (card == 12) {
-            System.out.println("Your card is a QUEEN!");
-            System.out.println("Your hand is " + totalHandFaceCards + "!");
-        } else if (card == 13) {
-            System.out.println("Your card is a KING!");
-            System.out.println("Your hand is " + totalHandFaceCards + "!");
-        }
+            int totalHandFaceCards = 0;
+            totalHandFaceCards = totalHandAces + 10; // adds 10 to player's hand
 
-        // Setting face card values after first draw
-        int Ace = 1;
-        int Jack = 10;
-        int Queen = 10;
-        int King = 10;
+            int handAceCard = 0;
+            handAceCard = totalHandAces + 1; // adds 1 to player's hand
 
-        System.out.println("1. Get another card");
-        System.out.println("2. Hold hand");
-        System.out.println("3. Print statistics");
-        System.out.println("4. Exit");
-
-        System.out.println("Choose an option: ");
-        Scanner scan = new Scanner(System.in);
-        int option = scan.nextInt();
+            int playerWins = 0;
+            int dealerWins = 0;
+            int playersTie = 0;
 
 
-        switch (option) {
-            case 1:
-                Random hand2 = new Random();
-                int card2 = hand2.nextInt((13 - 1) + 1) + 1;
+            if (card == 1) {
+                System.out.println("Your card is a ACE!");
+                System.out.println("Your hand is " + handAceCard + "!");
+            } else if ((card > 1) && (card < 11)) {
+                System.out.println("Your card is a " + card + "!");
+                System.out.println("Your hand is " + totalHand + "!");
+            } else if (card == 11) {
+                System.out.println("Your card is a JACK!");
+                System.out.println("Your hand is " + totalHandFaceCards + "!");
+            } else if (card == 12) {
+                System.out.println("Your card is a QUEEN!");
+                System.out.println("Your hand is " + totalHandFaceCards + "!");
+            } else if (card == 13) {
+                System.out.println("Your card is a KING!");
+                System.out.println("Your hand is " + totalHandFaceCards + "!");
+            }
 
-                int totalHand2 = 0;
-                totalHand2 = totalHand2 + card2 + card;
+            // Setting face card values after first draw
+            int Ace = 1;
+            int Jack = 10;
+            int Queen = 10;
+            int King = 10;
 
-                System.out.println("Your card is a " + card2 + "!");
-                System.out.println("Your hand is " + totalHand2 + "!");
+            int option = 0;
+            while (option != 4) {
 
-                if (totalHand2 == 21) {
-                    System.out.println("BLACKJACK! You win!");
-                    playerWins++;
-                    numGame++;
-                } else if (totalHand2 > 21) {
-                    System.out.println("You exceeded 21!! You lose :(");
-                    dealerWins++;
-                    numGame++;
-                } else {
-                    System.out.println("1. Get another card");
-                    System.out.println("2. Hold hand");
-                    System.out.println("3. Print statistics");
-                    System.out.println("4. Exit");
+                System.out.println("1. Get another card");
+                System.out.println("2. Hold hand");
+                System.out.println("3. Print statistics");
+                System.out.println("4. Exit");
 
-                    System.out.println("Choose an option: ");
-                }
-                    int option2 = scan.nextInt();
+                System.out.println("Choose an option: ");
+                Scanner scan = new Scanner(System.in);
+                option = scan.nextInt();
 
-                    if (option2 == 1) {
-                        Random hand3 = new Random();
-                        int card3 = hand3.nextInt((13 - 1) + 1) + 1;
+                boolean gameOver = false;
 
-                        int totalHand3 = 0;
-                        totalHand3 = totalHand2 + card2 + card + card;
+                while (gameOver == false) {
 
-                        System.out.println("Your card is a " + card3 + "!");
-                        System.out.println("Your hand is " + totalHand3 + "!");
+                    switch (option) {
+                        case 1:
+                            Random hand2 = new Random();
+                            int card2 = hand2.nextInt((13 - 1) + 1) + 1;
 
-                            if (totalHand3 == 21) {
+                            int totalHand2 = 0;
+                            totalHand2 = totalHand2 + card2 + card;
+
+                            System.out.println("Your card is a " + card2 + "!");
+                            System.out.println("Your hand is " + totalHand2 + "!");
+
+                            if (totalHand2 == 21) {
                                 System.out.println("BLACKJACK! You win!");
                                 playerWins++;
                                 numGame++;
-                            } else if (totalHand3 > 21) {
+                                gameOver = true;
+                            } else if (totalHand2 > 21) {
                                 System.out.println("You exceeded 21!! You lose :(");
                                 dealerWins++;
                                 numGame++;
-                            } else {
-                                System.out.println("1. Get another card");
-                                System.out.println("2. Hold hand");
-                                System.out.println("3. Print statistics");
-                                System.out.println("4. Exit");
+                                gameOver = true;
                             }
-                    }
+                            break;
 
-                    case 2:
+                        case 2:
                             Random holdHand = new Random();
                             int dealerHand = holdHand.nextInt((26 - 16) + 1) + 16;
 
@@ -129,18 +107,22 @@ public class Main {
                                 System.out.println("You win!");
                                 playerWins++;
                                 numGame++;
+                                gameOver = true;
                             } else if (dealerHand == totalHand) {
                                 System.out.println("It's a tie! No one wins!");
                                 playersTie++;
                                 numGame++;
+                                gameOver = true;
                             } else if (dealerHand > totalHand) {
                                 System.out.println("Dealer wins!");
                                 dealerWins++;
                                 numGame++;
+                                gameOver = true;
                             } else if (totalHand > dealerHand) {
                                 System.out.println("You win!");
                                 playerWins++;
                                 numGame++;
+                                gameOver = true;
                             }
                             break;
 
@@ -150,28 +132,32 @@ public class Main {
                             System.out.println("Number of tie games: " + playersTie);
                             System.out.println("Total # of games played is: " + numGame);
 
-                            double decimalPlayWins = ((playerWins)/(numGame));
+                            double decimalPlayWins = ((playerWins) / (numGame));
 
-                           double percentPlayWins = decimalPlayWins * 100;
-                           int percentPlayWinners = (int)percentPlayWins; // casting for the percentage
+                            double percentPlayWins = decimalPlayWins * 100;
+                            int percentPlayWinners = (int) percentPlayWins; // casting for the percentage
 
 
                             System.out.println("Percentage of Player wins: " + percentPlayWinners + "%");
                             break;
 
                         case 4:
-                            break;
+                            continuePlaying = false;
+                            return;
 
                         default:
                             System.err.println("Invalid input!");
                             System.err.println("Please enter an integer value between 1 and 4.");
-                            return;
+                            break;
 
                     }
 
 
                 }
+            }
         }
+    }
+}
 
 
 
